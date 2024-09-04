75be1258eb8c130e8b1d83b483b3facc9c40866d
Abhishek Thakur <abhishekthakur1050@gmail.com>
2024-09-04 17:19:17 +0530

    Remove console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side')
-
 const weatherForm = document.querySelector('form')
 const search= document.querySelector('input')
 const messageOne = document.querySelector('#message-1')

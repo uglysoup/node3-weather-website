218c456febad2688c141775af145fd13108b708e
Abhishek Thakur <abhishekthakur1050@gmail.com>
2024-09-04 19:41:21 +0530

    setup for Heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
+const port = process.env.PORT||3000
+
 //Define paths for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewPath= path.join(__dirname,'../template/views')
@@ -91,6 +93,6 @@ res.render('404',{
     errorMessage:'Page not found.'
 })
 })
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000.')
+app.listen(port, ()=>{
+    console.log('Server is up on port '+port)
 })
